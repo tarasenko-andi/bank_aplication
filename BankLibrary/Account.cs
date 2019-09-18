71cae85011d49d848767243348e6a93b35573c9a
tarasenko-andi <anditarasenko@gmail.com>
2019-09-18 21:25:23 +0200

    Add file project

---
+++ BankLibrary/Account.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public abstract class Account : IAccount
+    {
+        //Событие, возникающее при выводе денег
+        protected internal event AccountStateHandler Withdrawed;
+        // Событие возникающее при добавление на счет
+        protected internal event AccountStateHandler Added;
+        // Событие возникающее при открытии счета
+        protected internal event AccountStateHandler Opened;
+        // Событие возникающее при закрытии счета
+        protected internal event AccountStateHandler Closed;
+        // Событие возникающее при начислении процентов
+        protected internal event AccountStateHandler Calculated;
+        // Событие возникающее при просмотре счета
+        protected internal event AccountStateHandler Checked;
+        //Событие для пересылки денег
+        protected internal event AccountStateHandler Sended;
+
+        protected int _id;
+        static int counter = 0;
+
+        protected decimal _sum; // Переменная для хранения суммы
+        protected int _percentage; // Переменная для хранения процента
+
+        protected int _days = 0; // время с момента открытия счета
+
+        public Account(decimal sum, int percentage)
+        {
+            _sum = sum;
+            _percentage = percentage;
+            _id = ++counter;
+        }
+
+        // Текущая сумма на счету
+        public decimal CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public int Percentage
+        {
+            get { return _percentage; }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+        // вызов событий
+        private void CallEvent(AccountEventArgs e, AccountStateHandler handler)
+        {
+            if (handler != null && e != null)
+                handler(this, e);
+        }
+        // вызов отдельных событий. Для каждого события определяется свой витуальный метод
+        protected virtual void OnOpened(AccountEventArgs e)
+        {
+            CallEvent(e, Opened);
+        }
+        protected virtual void OnWithdrawed(AccountEventArgs e)
+        {
+            CallEvent(e, Withdrawed);
+        }
+        protected virtual void OnAdded(AccountEventArgs e)
+        {
+            CallEvent(e, Added);
+        }
+        protected virtual void OnClosed(AccountEventArgs e)
+        {
+            CallEvent(e, Closed);
+        }
+        protected virtual void OnCalculated(AccountEventArgs e)
+        {
+            CallEvent(e, Calculated);
+        }
+        protected virtual void OnChecked(AccountEventArgs e)
+        {
+            CallEvent(e, Checked);
+        }
+        protected virtual void OnSended(AccountEventArgs e)
+        {
+            CallEvent(e, Sended);
+        }
+
+        public virtual void Put(decimal sum)
+        {
+            _sum += sum;
+            OnAdded(new AccountEventArgs("На счет" + _id + " поступило " + sum, sum));
+        }
+
+
+        public virtual decimal Withdraw(decimal sum)
+        {
+            decimal result = 0;
+            if (sum <= _sum)
+            {
+                _sum -= sum;
+                result = sum;
+                OnWithdrawed(new AccountEventArgs("Сумма " + sum + " снята со счета " + _id, sum));
+            }
+            else
+            {
+                OnWithdrawed(new AccountEventArgs("Недостаточно денег на счете " + _id, 0));
+            }
+            return result;
+        }
+        // открытие счета
+        protected internal virtual void Open()
+        {
+            OnOpened(new AccountEventArgs("Открыт новый счет! Id счета: " + this._id, this._sum));
+        }
+        // закрытие счета
+        protected internal virtual void Close()
+        {
+            OnClosed(new AccountEventArgs("Счет " + _id + " закрыт.  Итоговая сумма: " + CurrentSum, CurrentSum));
+        }
+        // проверка суммы на счете
+        protected internal virtual void Check()
+        {
+            OnChecked(new AccountEventArgs("На счете " + _id + " в данный момент" + CurrentSum, CurrentSum));
+
+        }
+        protected internal virtual void Send()
+        {
+            OnSended(new AccountEventArgs("На счетe: " + _id + " " + CurrentSum, CurrentSum));
+        }
+
+        protected internal void IncrementDays()
+        {
+            _days++;
+        }
+        // начисление процентов
+        protected internal virtual void Calculate()
+        {
+            decimal increment = _sum * _percentage / 100;
+            _sum = _sum + increment;
+            OnCalculated(new AccountEventArgs("Начислены проценты в размере: " + increment + " клиенту" + _id, increment));
+        }
+
+    }
+}
